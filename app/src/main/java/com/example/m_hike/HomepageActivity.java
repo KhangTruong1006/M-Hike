d521f4605b0c13420ffebde6335f4da4d4c7922d
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-19 11:06:09 +0700

    new method for add button

--- app/src/main/java/com/example/m_hike/HomepageActivity.java
+++ app/src/main/java/com/example/m_hike/HomepageActivity.java
@@ -1,6 +1,8 @@
 package com.example.m_hike;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class HomepageActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void startHikeActivity(View view){
+        Intent intent = new Intent(this,HikeActivity.class);
+        startActivity(intent);
     }
 }
