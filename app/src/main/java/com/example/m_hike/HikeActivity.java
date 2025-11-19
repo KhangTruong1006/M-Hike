68558cd0274aec00cad389652e57428617237437
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-19 11:02:56 +0700

    new activity for displaying Hikes

---
+++ app/src/main/java/com/example/m_hike/HikeActivity.java
@@ -0,0 +1,42 @@
+package com.example.m_hike;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HikeActivity extends AppCompatActivity {
+    RecyclerView.Adapter hikeAdapter;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hike);
+
+        recyclerView = findViewById(R.id.rvHike);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DatabaseHelper db = new DatabaseHelper(this);
+        ArrayList<Hike> hikes = db.getHikeDetails();
+
+        hikeAdapter = new HikeAdapter(hikes);
+        recyclerView.setAdapter(hikeAdapter);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
