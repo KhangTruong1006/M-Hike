7eac5a3f6b1dc441140ecb0958a646b35fd9df71
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-19 17:10:33 +0700

    new function for add button

--- app/src/main/java/com/example/m_hike/HikeActivity.java
+++ app/src/main/java/com/example/m_hike/HikeActivity.java
@@ -1,6 +1,8 @@
 package com.example.m_hike;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,5 +40,10 @@ public class HikeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void clickAddButton(View view){
+        Intent intent = new Intent(this, NewHikeActivity.class);
+        startActivity(intent);
     }
 }
