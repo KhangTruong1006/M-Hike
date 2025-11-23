23b6141e75309ddb0a5ddaab4600221d6a8b46dd
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-23 16:24:21 +0700

    new dependency of material card view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.fragment)
     implementation(libs.cardview)
     implementation(libs.recyclerview)
+    implementation(libs.androidx.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
