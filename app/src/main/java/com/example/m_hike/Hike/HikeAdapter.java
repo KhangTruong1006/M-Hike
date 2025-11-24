1b5c188b9463421051a4a75c200dbb9090213f30
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-24 21:10:14 +0700

    new methods for handling button in each card

--- app/src/main/java/com/example/m_hike/Hike/HikeAdapter.java
+++ app/src/main/java/com/example/m_hike/Hike/HikeAdapter.java
@@ -1,14 +1,18 @@
 package com.example.m_hike.Hike;
 
+import android.content.Context;
+import android.content.Intent;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.m_hike.EditHikeActivity;
 import com.example.m_hike.R;
 
 import org.jspecify.annotations.NonNull;
@@ -33,13 +37,52 @@ public class HikeAdapter extends RecyclerView.Adapter<HikeAdapter.HikeViewHolder
         holder.tvHikeName.setText(current.getName());
         holder.tvDate.setText(current.getDate());
         holder.tvLocation.setText(current.getLocation());
-        holder.tvLength.setText(current.getLength() + "km");
+        holder.tvLength.setText(current.getLength() + " km");
         holder.tvDifficulty.setText(current.getDifficulty());
 
         String parking = (current.getParking() == 1)? "Available" : "Unavailable";
         holder.tvParking.setText(parking);
 
+        String completion = (current.getCompleted() == 1)? "Completed" : "Not Completed";
+        holder.tvCompletion.setText(completion);
 
+
+        setFavoriteButton(holder, current);
+        enableDescription(holder,current);
+
+        clickEditButton(holder,current);
+
+
+//        clickFavoriteButton(holder, current);
+
+    }
+
+    @Override
+    public int getItemCount(){return hikes.size();}
+
+    public class HikeViewHolder extends RecyclerView.ViewHolder{
+        public TextView tvHikeName, tvDate, tvLocation, tvParking, tvLength, tvDifficulty,tvCompletion,tvDescription;
+        public LinearLayout itemHikeLayout;
+        public ImageButton btn_favorite, btn_edit_hike;
+
+        public HikeViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvHikeName = itemView.findViewById(R.id.tv_item_hike_name);
+            tvDate = itemView.findViewById(R.id.tv_item_hike_date);
+            tvLocation = itemView.findViewById(R.id.tv_item_hike_location);
+            tvParking = itemView.findViewById(R.id.tv_item_hike_parking);
+            tvLength = itemView.findViewById(R.id.tv_item_hike_length);
+            tvDifficulty = itemView.findViewById(R.id.tv_item_hike_difficulty);
+            tvCompletion = itemView.findViewById(R.id.tv_item_hike_completion);
+            tvDescription = itemView.findViewById(R.id.tv_item_hike_description);
+            itemHikeLayout = itemView.findViewById(R.id.item_hike_layout);
+            btn_favorite = itemView.findViewById(R.id.btn_favorite);
+            btn_edit_hike = itemView.findViewById(R.id.btn_edit_hike);
+
+        }
+    }
+
+    private void enableDescription(@NonNull HikeViewHolder holder, Hike current){
         if(!current.getDescription().equalsIgnoreCase("")) {
             holder.tvDescription.setText(current.getDescription());
             holder.itemHikeLayout.setOnClickListener(new View.OnClickListener() {
@@ -55,24 +98,39 @@ public class HikeAdapter extends RecyclerView.Adapter<HikeAdapter.HikeViewHolder
         }
     }
 
-    @Override
-    public int getItemCount(){return hikes.size();}
+    private void clickEditButton(@NonNull HikeViewHolder holder, Hike current){
+        holder.btn_edit_hike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, EditHikeActivity.class);
+                intent.putExtra("HIKE_ID", current.getId());
+                context.startActivity(intent);
+            }
+        });
+    }
 
-    public class HikeViewHolder extends RecyclerView.ViewHolder{
-        public TextView tvHikeName, tvDate, tvLocation, tvParking, tvLength, tvDifficulty,tvDescription;
-        public LinearLayout itemHikeLayout;
-
-        public HikeViewHolder(@NonNull View itemView){
-            super(itemView);
-            tvHikeName = itemView.findViewById(R.id.tv_item_hike_name);
-            tvDate = itemView.findViewById(R.id.tv_item_hike_date);
-            tvLocation = itemView.findViewById(R.id.tv_item_hike_location);
-            tvParking = itemView.findViewById(R.id.tv_item_hike_parking);
-            tvLength = itemView.findViewById(R.id.tv_item_hike_length);
-            tvDifficulty = itemView.findViewById(R.id.tv_item_hike_difficulty);
-            tvDescription = itemView.findViewById(R.id.tv_item_hike_description);
-            itemHikeLayout = itemView.findViewById(R.id.item_hike_layout);
-
+    private void setFavoriteButton(@NonNull HikeViewHolder holder, Hike current){
+        if(current.getFavorite() == 0){
+            holder.btn_favorite.setImageResource(R.drawable.favorite_24dp_b7b7b7_fill0_wght400_grad0_opsz24);
+        }
+        else{
+            holder.btn_favorite.setImageResource(R.drawable.favorite_24dp_ea3323_fill1_wght400_grad0_opsz24);
         }
     }
+
+//    public void clickFavoriteButton(@NonNull HikeViewHolder holder, Hike current){
+//        holder.btn_favorite.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(current.getFavorite() == 0){
+//                    current.setFavorite(1);
+//                    holder.btn_favorite.setImageResource(R.drawable.favorite_24dp_b7b7b7_fill0_wght400_grad0_opsz24);
+//                } else {
+//                    current.setFavorite(0);
+//                    holder.btn_favorite.setImageResource(R.drawable.favorite_24dp_ea3323_fill1_wght400_grad0_opsz24);
+//                }
+//            }
+//        });
+//    }
 }
