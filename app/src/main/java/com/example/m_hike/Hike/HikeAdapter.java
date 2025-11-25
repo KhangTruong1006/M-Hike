d50854179beb19087f432647c686241ca7c956c9
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-25 23:15:44 +0700

    refactor import

--- app/src/main/java/com/example/m_hike/Hike/HikeAdapter.java
+++ app/src/main/java/com/example/m_hike/Hike/HikeAdapter.java
@@ -3,7 +3,6 @@ package com.example.m_hike.Hike;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +14,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.m_hike.DatabaseHelper.DatabaseHelper;
-import com.example.m_hike.EditHikeActivity;
 import com.example.m_hike.R;
 
 import org.jspecify.annotations.NonNull;
