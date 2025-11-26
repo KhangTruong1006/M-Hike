57ec17225b1532a356ae308f44d276df95d6bd08
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-26 17:06:27 +0700

    new function for refreshing recyclerview activity and saving new activity

--- app/src/main/java/com/example/m_hike/Observation/ObservationActivity.java
+++ app/src/main/java/com/example/m_hike/Observation/ObservationActivity.java
@@ -1,26 +1,69 @@
 package com.example.m_hike.Observation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.m_hike.DatabaseHelper.DatabaseHelper;
 import com.example.m_hike.R;
+
+import java.util.ArrayList;
 
 public class ObservationActivity extends AppCompatActivity {
 
+    RecyclerView.Adapter observationAdapter;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    private DatabaseHelper db;
+    private int hike_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_observation);
+
+        Intent intent = getIntent();
+        hike_id = intent.getIntExtra("HIKE_ID",0);
+
+        recyclerView = findViewById(R.id.rvObservation);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+        db = new DatabaseHelper(this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void refreshRecyclerView(){
+        ArrayList<Observation> observations = db.getObservation(hike_id);
+        observationAdapter = new ObservationAdapter(observations,db);
+        recyclerView.setAdapter(observationAdapter);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        refreshRecyclerView();
+    }
+
+    public void clickNewObservationButton(View view){
+        Intent intent = new Intent(this,ObservationNewActivity.class);
+        intent.putExtra("HIKE_ID",hike_id);
+        startActivity(intent);
     }
 }
