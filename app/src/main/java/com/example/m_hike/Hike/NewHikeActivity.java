ee7908dc9f188f87371401363fe3863b2d035d2d
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-11-30 21:05:58 +0700

    modify date input field by letting user choose date from calendar

--- app/src/main/java/com/example/m_hike/Hike/NewHikeActivity.java
+++ app/src/main/java/com/example/m_hike/Hike/NewHikeActivity.java
@@ -1,8 +1,10 @@
 package com.example.m_hike.Hike;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -16,7 +18,11 @@ import com.example.m_hike.DatabaseHelper.DatabaseHelper;
 import com.example.m_hike.Helper.Helper;
 import com.example.m_hike.R;
 
-public class NewHikeActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class NewHikeActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     private EditText input_name, input_location, input_date, input_length, input_description;
     private CheckBox cb_parking;
@@ -40,11 +46,40 @@ public class NewHikeActivity extends AppCompatActivity {
         cb_parking = findViewById(R.id.cb_parking);
         spinner_difficulty = findViewById(R.id.spinner_difficulty);
 
+        input_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    private void showDatePickerDialog(){
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,this,year,month,day);
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year , int month, int day){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String selectedDate = sdf.format(calendar.getTime());
+
+        input_date.setText(selectedDate);
     }
     private void saveHikeDetails(){
         DatabaseHelper dbHelper = new DatabaseHelper(this);
@@ -87,4 +122,6 @@ public class NewHikeActivity extends AppCompatActivity {
         }
         return false;
     }
+
+
 }
